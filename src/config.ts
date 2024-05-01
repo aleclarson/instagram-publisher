053ce1a9df1eca1e1ea2c66241a07a43edaf24c8
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-05-01 13:33:44 -0400

    wip

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,4 @@ export const MAX_SLIDESHOW_IMAGES = 10;
 export const VALID_VIDEO_ASPECT_RATIOS = ['1:1', '16:9', '9:16'];
 export const VALID_VIDEO_EXTENSION = ['mp4'];
 
-export const COOKIES_FILE_PATH = 'cookies.json';
 export const BASE_URL = 'https://i.instagram.com';
--- src/shared.ts
+++ src/shared.ts
@@ -1,20 +1,10 @@
-import * as fs from 'fs';
-import { COOKIES_FILE_PATH } from './config';
-
-export function validateCookies(): Boolean {
-  const exists = fs.existsSync(COOKIES_FILE_PATH);
-
-  // check expiration date
-  if (exists) {
-    const cookies = JSON.parse(fs.readFileSync(COOKIES_FILE_PATH, 'utf8'));
-    const expirationDate = new Date(
-      cookies.find((c: any) => c.key === 'sessionid').expires
-    );
-    return new Date() < expirationDate;
-  }
-  return false;
+export function validateCookies(cookies: any[]): Boolean {
+  const expirationDate = new Date(
+    cookies.find((c: any) => c.key === 'sessionid').expires
+  );
+  return new Date() < expirationDate;
 }
 
 export async function sleep(milliseconds: number) {
-  await new Promise(resolve => setTimeout(resolve, milliseconds));
+  await new Promise((resolve) => setTimeout(resolve, milliseconds));
 }
